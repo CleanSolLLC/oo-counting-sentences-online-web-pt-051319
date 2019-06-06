93abbbbd9c208f6949d1a5ee9d2872e02199a19a
Mark May <mamark90@gmail.com>
2019-06-06 12:37:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    sentences = self.split((?:\.|\?|\!)(?= [^a-z]))
+    sentences = self.split(?:\.|\?|\!)(?= [^a-z])
     sentences.count
     binding.pry
   end
